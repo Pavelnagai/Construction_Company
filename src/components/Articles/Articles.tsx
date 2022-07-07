6692445c4356462ed06360e4d462bd427aaf2589
Pavelnagai <pavel.nagai@mail.ru>
2022-07-07 14:11:13 +0300

    adaptive

--- src/components/Articles/Articles.tsx
+++ src/components/Articles/Articles.tsx
@@ -3,34 +3,34 @@ import cla from '../../image/Project Cover.png'
 import bet from '../../image/Project Cover-1.png'
 import './Articles.scss'
 
+type ArticleType = {
+    articleTitle: string,
+    titleTwo: string
+    src: any,
+
+}
+
+function Article(props: ArticleType) {
+    return <div className="container col-sm-12 col-md-6 mb-5 article">
+        <h3>{props.articleTitle}</h3>
+        <img className="img-fluid" src={props.src} alt=""/>
+        <div className="row mt-4">
+            <div className="col-6 d-none d-sm-block"><h6>{props.titleTwo}</h6></div>
+            <div className="col-6 text-center btnDiv ">
+                <button className="btn btnWar btnWindow">Читать полностью</button>
+            </div>
+        </div>
+    </div>;
+}
+
 const Articles = () => {
     return (
-        <div className="articles">
-            <div className="container-fluid">
-                <div className="container">
-                    <h2>Актульные статьи</h2>
-                    <div className="row">
-                        <div className="col-sm-12 col-md-6">
-                            <h3>Как определяется класс бетона?</h3>
-                            <img className="img-fluid" src={cla} alt=""/>
-                            <div className="row">
-                                <div className="col-6"><h6>Классы бетона</h6></div>
-                                <div className="col-6">
-                                    <button className="btn btn-warning">Читать полностью</button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-sm-12 col-md-6">
-                            <h3>Марки бетона: характеристики</h3>
-                            <img className="img-fluid" src={bet} alt=""/>
-                            <div className="row">
-                                <div className="col-6"><h6>Марки бетона</h6></div>
-                                <div className="col-6">
-                                    <button className="btn btn-warning">Читать полностью</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+        <div className="container-fluid articles">
+            <div className="container">
+                <h2>Актульные статьи</h2>
+                <div className="row">
+                    <Article articleTitle="Марки бетона: характеристики" src={cla} titleTwo="Марки бетона"/>
+                    <Article articleTitle="Как определяется класс бетона?" src={bet} titleTwo="Классы бетона"/>
                 </div>
             </div>
         </div>
