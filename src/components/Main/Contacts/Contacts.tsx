4762041c083b2ce38adf8e35d26c92557e98f31e
Pavelnagai <pavel.nagai@mail.ru>
2022-07-07 10:06:51 +0300

    refactoring

--- src/components/Main/Contacts/Contacts.tsx
+++ src/components/Main/Contacts/Contacts.tsx
@@ -7,22 +7,24 @@ import FormContacts from '../../Forms/FormContacts/FormContacts';
 
 const Contacts = () => {
     return (
-        <div className="container pb-5 contacts">
-            <div className="row pt-5">
-                <div className="col-md-5">
-                    <h2>Контакты</h2>
-                    <p className="d-md-none text-start textSm">Собственные бетонные заводы</p>
-                    <MyComponent/>
+        <div className="contacts">
+            <div className="container pb-5">
+                <div className="row pt-5">
+                    <div className="col-md-5">
+                        <h2>Контакты</h2>
+                        <p className="d-md-none text-start textSm">Собственные бетонные заводы</p>
+                        <MyComponent/>
+                    </div>
+                    <div className="formContactsSM">
+                        <FormContacts/>
+                    </div>
+                    <div className="col-md-7">
+                        <FormQuestions titleButton="Заказать звонок"/>
+                    </div>
                 </div>
-                <div className="formContactsSM">
+                <div className="d-none d-md-block">
                     <FormContacts/>
                 </div>
-                <div className="col-md-7">
-                    <FormQuestions titleButton="Заказать звонок"/>
-                </div>
-            </div>
-            <div className="d-none d-md-block">
-                <FormContacts/>
             </div>
         </div>
     );
