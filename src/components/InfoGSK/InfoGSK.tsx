d7d662cd11b6718c8ed7ecd86ff37c9fd02ec1e9
Pavelnagai <pavel.nagai@mail.ru>
2022-07-08 07:35:24 +0300

    InfoGsk adaptive

--- src/components/InfoGSK/InfoGSK.tsx
+++ src/components/InfoGSK/InfoGSK.tsx
@@ -1,28 +1,27 @@
 import React from 'react';
-import constructor from "../../image/workGSK/builder-construction-vest-orange-helmet-standing-white-studio 1.png";
+import constructor from "../../image/Project Cover.svg"
+import constructor2 from "../../image/svg/Ellipse 46.svg"
 import './InfoGSK.scss';
+
 const InfoGsk = () => {
     return (
-        <div className="infoGsk">
-        <div className="container-fluid">
-            <div>
-                <div className="row d-flex align-items-center flex-sm-row" >
-                    <div className="col-sm-3 col-md-6">
-                        <img className="img-fluid" src={constructor} alt="constructor"/>
-                    </div>
-                    <div className="col-sm-8 col-md-6">
-                        <div>
-                            <h3>О ГСК</h3>
-                        </div>
-                        <p className="text-sm-start">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Condimentum diam orci pretium a
-                            pharetra, feugiat cursus. Dictumst risus, sem egestas odio cras adipiscing vulputate. Nisi,
-                            risus in suscipit non. Non commodo volutpat, pharetra, vel.</p>
-                    </div>
-                </div>
+        <div className="infoGsk row">
+            <div className="col-md-6">
+                <img className="img-fluid d-none d-sm-block" src={constructor} alt="constructor"/>
+                <img className="pt-2 imgSm" style={{position: "absolute", left: "20px"}} src={constructor2} alt=""/>
             </div>
-        </div>
+
+            <div className="col-md-4 d-flex flex-column justify-content-center">
+                <h2 className="text-center text-md-start h2InfoGsk p-2 p-md-0">О ГСК</h2>
+                <p className="text-sm-start textDecor">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Condimentum diam orci pretium a
+                    pharetra, feugiat cursus. Dictumst risus, sem egestas odio cras adipiscing vulputate.
+                    Nisi,
+                    risus in suscipit non. Non commodo volutpat, pharetra, vel.</p>
+            </div>
         </div>
     );
 };
 
 export default InfoGsk;
+
